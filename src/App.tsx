7f73e3fec4179b092690635546d934156451727c
Javier Ruzaafa <javier.reyes@adidas.com>
2021-12-10 16:07:39 +0100

    Invoice list example

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface IInvoiceListProps {
+  invoiceData: InvoiceDataType;
+  logo?: string;
+}
+
+type InvoiceDataType = {
+  customerName: string;
+  invoices:InvoiceType[];
+}
+
+type PaymentStatusType = "paid" | "pending" | "late";
+
+type InvoiceType = {
+  id: number; 
+  name: string; 
+  total: string;
+  paymentStatus: PaymentStatusType; // directly can be added here -> "paid" | "pending";
+}
+const InvoiceList = (props:IInvoiceListProps) => {
+  const {customerName, invoices} = props.invoiceData;
+  const itemStyleContainer = {display: "flex", justifyContent: "space-between"};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{customerName}</h1>
+      <hr />
+      <div>
+        {invoices.map((invoice) => (
+          <div key={invoice.id} style={itemStyleContainer}>
+            <div>{invoice.name}</div>
+            <div>{invoice.total} - {invoice.paymentStatus}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const data :InvoiceDataType = {
+    customerName: 'Google',
+    invoices: [
+      {id: 123, name: "Dev Work", total: "20.00", paymentStatus: "paid"},
+      {id: 456, name: "More Dev Work", total: "50.00", paymentStatus:"pending"},
+      {id: 789, name: "Other matters", total: "100.00", paymentStatus:"paid"}
+    ]
+  }
+  return (
+    <div className="App">
+      <InvoiceList invoiceData={data} />
     </div>
   );
 }
