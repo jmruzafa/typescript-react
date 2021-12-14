5e34671f650b61ea932a583ec5592314ae68c794
Javier Ruzaafa <javier.reyes@adidas.com>
2021-12-14 22:36:47 +0100

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 interface IInvoiceListProps {
   invoiceData: InvoiceDataType;
@@ -9,20 +9,23 @@ interface IInvoiceListProps {
 
 type InvoiceDataType = {
   customerName: string;
-  invoices:InvoiceType[];
-}
+  invoices: InvoiceType[];
+};
 
 type PaymentStatusType = "paid" | "pending" | "late";
 
 type InvoiceType = {
-  id: number; 
-  name: string; 
+  id: number;
+  name: string;
   total: string;
   paymentStatus: PaymentStatusType; // directly can be added here -> "paid" | "pending";
-}
-const InvoiceList = (props:IInvoiceListProps) => {
-  const {customerName, invoices} = props.invoiceData;
-  const itemStyleContainer = {display: "flex", justifyContent: "space-between"};
+};
+const InvoiceList = (props: IInvoiceListProps) => {
+  const { customerName, invoices } = props.invoiceData;
+  const itemStyleContainer = {
+    display: "flex",
+    justifyContent: "space-between",
+  };
   return (
     <div className="App">
       <h1>{customerName}</h1>
@@ -31,23 +34,35 @@ const InvoiceList = (props:IInvoiceListProps) => {
         {invoices.map((invoice) => (
           <div key={invoice.id} style={itemStyleContainer}>
             <div>{invoice.name}</div>
-            <div>{invoice.total} - {invoice.paymentStatus}</div>
+            <div>
+              {invoice.total} - {invoice.paymentStatus}
+            </div>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 function App() {
-  const data :InvoiceDataType = {
-    customerName: 'Google',
+  const data: InvoiceDataType = {
+    customerName: "Google",
     invoices: [
-      {id: 123, name: "Dev Work", total: "20.00", paymentStatus: "paid"},
-      {id: 456, name: "More Dev Work", total: "50.00", paymentStatus:"pending"},
-      {id: 789, name: "Other matters", total: "100.00", paymentStatus:"paid"}
-    ]
-  }
+      { id: 123, name: "Dev Work", total: "20.00", paymentStatus: "paid" },
+      {
+        id: 456,
+        name: "More Dev Work",
+        total: "50.00",
+        paymentStatus: "pending",
+      },
+      {
+        id: 789,
+        name: "Other matters",
+        total: "100.00",
+        paymentStatus: "paid",
+      },
+    ],
+  };
   return (
     <div className="App">
       <InvoiceList invoiceData={data} />
